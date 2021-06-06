c8abc56583b90115b6f5852adc1117d6b7365151
ossyahya60 <ossyahya60@gmail.com>
2021-06-06 11:25:16 +0200

    Added Robots.txt Handler and fixed some issues

--- Search_Engine/src/CrawlerMain.java
+++ Search_Engine/src/CrawlerMain.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -12,56 +19,66 @@ public class CrawlerMain implements Runnable {
 	
 	static BlockingQueue<String> URLQueue;
 	static FileWriter SerializedData;
+	static HashMap<String, ArrayList<String>> RobotContainer = new HashMap<String, ArrayList<String>>();
 	
 	static int Counter = 0;
-	final static int crawlsCount = 5000;
+	static AtomicInteger DisallowedURLs = new AtomicInteger();
 
 	private void Crawl()
 	{	
-		while(Counter < crawlsCount)
+		while(Counter < 5000)
 		{
-			if(URLQueue.size() != 0)
+			String URL = "";
+			synchronized(URLQueue)
 			{
-				String URL = "";
-				synchronized(URLQueue)
-				{
-					if(URLQueue.size() > 0)
-						URL = URLQueue.remove();
-				}
+				while(URLQueue.size() == 0)
+				{}
 				
-				try {
-					Document document = null;
-					document = Jsoup.connect(URL).ignoreContentType(true).get();
-;
-					synchronized(SerializedData)
-					{
-						if(Counter >= crawlsCount)
-							return;
-						
-						SerializedData.write(URL + " AdhamNoice ");
-						SerializedData.write(shrinkDoc(document).replace("\n", "").replace("\r", "").replace(System.getProperty("line.separator"), "").replace("\r\n", "") + "\n");
-						
-						System.out.println(Counter++);
-					}
-		
-			        //3. Parse the HTML to extract links to other URLs
-			        Elements linksOnPage = document.select("a[href]");
+				URL = URLQueue.remove();
+			}
 			
-			        //5. For each extracted URL... go back to Step 4.
-			        for (Element page : linksOnPage)
-			        {
+			try {
+				if(HandleRobot(URL))
+					return;
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
+			
+			try {
+				Document document = null;
+				document = Jsoup.connect(URL).ignoreContentType(true).get();
+
+				synchronized(SerializedData)
+				{
+					if(Counter >= 5000)
+						return;
+					
+					SerializedData.write(URL + " AdhamNoice ");
+					SerializedData.write(shrinkDoc(document).replace("\n", "").replace("\r", "").replace(System.getProperty("line.separator"), "").replace("\r\n", "") + "\n");
+					
+					System.out.println(((Counter++)/5000.0f)*100 + "%");
+				}
+	
+		        //3. Parse the HTML to extract links to other URLs
+		        Elements linksOnPage = document.select("a[href]");
+		
+		        //5. For each extracted URL... go back to Step 4.
+		        for (Element page : linksOnPage)
+		        {
+		        	synchronized(URLQueue)
+		        	{
 		        		if(URLQueue.contains(page.attr("abs:href")))
 		        			continue;
-		        		URLQueue.add(page.attr("abs:href"));
-			        }
-				}
-				catch(Exception e)
-				{}
+		        	}
+	        		URLQueue.add(page.attr("abs:href"));
+		        }
 			}
+			catch(Exception e)
+			{}
 		}
 	}
 	
-	//Adham
 	static public String shrinkDoc(Document doc) 
 	{
 		org.jsoup.select.Elements ele;
@@ -84,22 +101,40 @@ public class CrawlerMain implements Runnable {
 		return sb.toString();
 	}
 	
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws InterruptedException, IOException
-	{			
-		
-		URLQueue = new ArrayBlockingQueue<>(crawlsCount);
-		
-        URLQueue.add("https://www.deepcrawl.com/blog/releases/log-file-integration/");
-        URLQueue.add("https://washingtonpress.com/");
-        URLQueue.add("https://proxycrawl.com/how-to-scrape-reddit");
-        URLQueue.add("https://www.quora.com/What-are-some-interesting-sites-to-web-crawl");
+	{	
+		/*
+		// Handling Interruption
+        Runtime.getRuntime().addShutdownHook(new Thread() {
 
+		    @Override
+		    public void run() {
+		        // place your code here
+		    	System.out.println("Exiting");
+		    }
 
-        Thread[] THREADS = new Thread[25];
+		});
+		*/
+		
+		URLQueue = new ArrayBlockingQueue<>(5000);
+		
+        URLQueue.add("https://stackoverflow.com/");					//Programming
+		URLQueue.add("https://www.theverge.com/tech"); 				//Tech
+        URLQueue.add("https://www.espn.com/");     					//Sports
+        URLQueue.add("https://www.health.com/");     				//Health
+        URLQueue.add("https://edition.cnn.com/politics");			//Politics
+        URLQueue.add("https://www.allrecipes.com/");				//Cooking
+        URLQueue.add("https://www.bbc.com/news/business/economy");	//Economy
+        URLQueue.add("https://www.imdb.com/");						//Entertainment
+        URLQueue.add("https://www.amazon.com/");					//Online Shopping
+        URLQueue.add("https://www.pcgamer.com/news/");				//Gaming
         
-        SerializedData = new FileWriter((String)(System.getProperty("user.dir") + "\\inventory\\CrawledData.json"));
+        Thread[] THREADS = new Thread[50];
+        
+		SerializedData = new FileWriter("/C:/APT/Project/CrawledData.json");
 
-        for(int i=0; i<25; i++)
+        for(int i=0; i<50; i++)
         {
         	while(URLQueue.size() == 0)
         	{}
@@ -109,17 +144,79 @@ public class CrawlerMain implements Runnable {
         	THREADS[i].start();
         }
         
-        for(int i=0; i<25; i++)
+        for(int i=0; i<50; i++)
         	THREADS[i].join();
                 
         SerializedData.close();
 		
         SerializedData = null;
-        
     }
 
 	@Override
 	public void run() {
+		// TODO Auto-generated method stub
 		Crawl();
+	}
+	
+	public boolean HandleRobot(String URL) throws MalformedURLException
+	{
+		String Host = new URL(URL).getHost();
+
+		synchronized(RobotContainer)
+		{
+			if(!RobotContainer.containsKey(Host))
+				RobotContainer.put(Host, ParseRobot(Host));
+		}
+
+		ArrayList<String> URLs = RobotContainer.get(Host);
+		
+		for (String DisallowedURL : URLs)
+			if(DisallowedURL.equals(new URL(URL).getPath()))
+				return true;
+				
+		return false;
+	}
+	
+	//This bit of code is fetched from stackoverflow (Modified to suit my needs)
+	//URL: https://stackoverflow.com/questions/25731346/reading-robot-txt-with-jsoup-line-by-line
+	public ArrayList<String> ParseRobot(String URL) {
+		
+		ArrayList<String> DisallowedURLs = new ArrayList<String>();	  
+		
+	    try(BufferedReader in = new BufferedReader(new InputStreamReader(new URL("https://" + URL + "/robots.txt").openStream()))) 
+	    {
+	        String line = null;
+	        boolean FoundUserAgent = false;
+
+	        while((line = in.readLine()) != null)
+	        {
+	            String[] SplittedLine = line.split(" ");
+	            
+	            if(SplittedLine.length <= 1)
+	            	continue;
+	            
+	            if(FoundUserAgent || (SplittedLine[0].equals("User-agent:") && SplittedLine[1].equals("*")))
+	            {	
+	            	if(!FoundUserAgent)
+	            	{
+	            		FoundUserAgent = true;
+	            		continue;
+	            	}
+	            	
+	            	if(SplittedLine[0].equals("Disallow:"))
+	            		DisallowedURLs.add(SplittedLine[1]);
+	            	else if(SplittedLine[0].equals("Allow:"))
+	            		continue;
+	            	else
+	            		break;
+	            }
+	        }	        
+	    }
+	    catch (IOException e) 
+	    {
+	        //e.printStackTrace();
+	    }
+	    
+        return DisallowedURLs;
 	}
 }
